2d059e643ce1c842efac3edbdd087274d67246b5
socratesk <socratesk@gmail.com>
2018-06-13 13:15:34 -0500

    Code clean-up

--- Predictor.py
+++ Predictor.py
@@ -1,4 +1,4 @@
-# import the Flask class from the flask module
+# Import Flask library from the flask module
 from flask import Flask, render_template, request, jsonify
 
 # REFERENCE: https://blog.keras.io/building-a-simple-keras-deep-learning-rest-api.html?__s=kiszvfzpzeidxjqjknxq
@@ -25,23 +25,23 @@ def upload():
 # Execute prediction function
 @app.route("/predict", methods=["POST"])
 def predict():
-    # initialize the data dictionary that will be returned from the view
+    # Initialize the data dictionary that will be returned from the view
 	data = {"success": False}
 
-    # ensure an image was properly uploaded to our endpoint
+    # Ensure an image was properly uploaded to our endpoint
 	if request.method == "POST":
 
 		print (request.files)
 		if request.files.get("file"):
 
-			# read the image in PIL format
+			# Read the image in PIL format
 			image = request.files["file"].read()
 			image = Image.open(io.BytesIO(image))
 
-			# preprocess the image and prepare it for classification
+			# Preprocess the image and prepare it for classification
 			image = prepare_image(image, target=(224, 224))
 
-			# classify the input image and then initialize the list
+			# Classify the input image and then initialize the list
 			# of predictions to return to the client
 			with graph.as_default():
 				preds = model.predict(image)
@@ -53,7 +53,7 @@ def predict():
 					r = {"label": label, "probability": float(prob)*100}
 					data["predictions"].append(r)
 
-				# indicate that the request was a success
+				# Indicate that the request was a success
 				data["success"] = True
 
 				if data["success"]:
@@ -68,32 +68,32 @@ def predict():
 					print (predStr)
 					data["predStr"] = predStr
 
-				# otherwise, the request failed
+				# Otherwise, the request failed
 				else:
 					print("Request failed")
 
-		# return the data dictionary as a JSON response
+		# Return the data dictionary as a JSON response
 		return (jsonify(data))
 
 
 
 def prepare_image(image, target):
-    # if the image mode is not RGB, convert it
+    # If the image mode is not RGB, convert it
     if image.mode != "RGB":
         image = image.convert("RGB")
 
-    # resize the input image and preprocess it
+    # Resize the input image and preprocess it
     image = image.resize(target)
     image = img_to_array(image)
     image = np.expand_dims(image, axis=0)
     image = imagenet_utils.preprocess_input(image)
 
-    # return the processed image
+    # Return the processed image
     return image
 
 
 def load_model():
-    # load the pre-trained Keras model (here we are using a model
+    # Load the pre-trained Keras model (here we are using a model
     # pre-trained on ImageNet and provided by Keras, but you can
     # substitute in your own networks just as easily)
 	global model
@@ -102,14 +102,14 @@ def load_model():
 	graph = tf.get_default_graph()
 
 
-# use decorators to link the function to a url
+# Use decorators to link the function to an url
 @app.route('/')
 def home():
     return "Hello, World!"  # return a string
 
 
-# start the server with the 'run()' method
+# Start the server with the 'run()' method
 if __name__ == "__main__":
 	print(("*Starting server... \n Please wait until server has fully started"))
-	#load_model()
+	# load_model()
 	app.run()
